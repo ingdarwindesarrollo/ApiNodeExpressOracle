aecc9f87be027dc58bea7f0c77b2abbe6b7d86bb
ingdarwindesarrollo <ingdarwin.desarrollo@gmail.com>
2026-03-22 09:05:57 -0500

    API REST con Node.js, Express y Oracle DB

---
+++ init-db.js
@@ -0,0 +1,40 @@
+const oracledb = require('oracledb');
+
+async function init() {
+    let conn;
+
+    try {
+        conn = await oracledb.getConnection({
+            user: 'system',
+            password: '123456',
+            connectString: 'localhost/XEPDB1'
+        });
+
+        console.log('✅ Conectado');
+
+        await conn.execute(`
+            BEGIN
+                EXECUTE IMMEDIATE '
+                    CREATE TABLE users (
+                        id NUMBER GENERATED ALWAYS AS IDENTITY,
+                        name VARCHAR2(100),
+                        email VARCHAR2(100),
+                        PRIMARY KEY (id)
+                    )
+                ';
+            EXCEPTION
+                WHEN OTHERS THEN
+                    IF SQLCODE != -955 THEN RAISE; END IF;
+            END;
+        `);
+
+        console.log('✅ Tabla creada');
+
+    } catch (err) {
+        console.error(err);
+    } finally {
+        if (conn) await conn.close();
+    }
+}
+
+init();
---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,31 @@
+const logger = require('../utils/logger');
+
+function errorHandler(err, req, res, next) {
+    let message = err.message;
+    let status = err.status || 500;
+
+    // 🔹 Oracle errores
+    if (err.message.includes('ORA-00001')) {
+        message = 'Registro duplicado';
+        status = 400;
+    }
+
+    if (err.message.includes('ORA-00942')) {
+        message = 'Tabla no existe';
+        status = 500;
+    }
+
+    if (err.message.includes('ORA-02291')) {
+        message = 'Violación de clave foránea';
+        status = 400;
+    }
+
+    logger.error(`${req.method} ${req.url} - ${message}`);
+
+    res.status(status).json({
+        success: false,
+        message
+    });
+}
+
+module.exports = errorHandler;
